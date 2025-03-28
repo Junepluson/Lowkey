4dc66a732b8f2f41afd04f5b70ca25d812a26ffe
Hong Seongjun <129278491+Junepluson@users.noreply.github.com>
2025-03-28 22:22:04 +0900

    Initial Commit

---
+++ Lowkey/LowkeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LowkeyApp.swift
+//  Lowkey
+//
+//  Created by seongjun Hong on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct LowkeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LowkeyTests/LowkeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  LowkeyTests.swift
+//  LowkeyTests
+//
+//  Created by seongjun Hong on 3/28/25.
+//
+
+import Testing
+@testable import Lowkey
+
+struct LowkeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
